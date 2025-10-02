cc30fbf9b182655145dca01a5a5cff7b3fcfa814
Daniel Olanrewaju <127168675+devfreeguy@users.noreply.github.com>
2025-10-02 23:09:45 +0100

    Update express.ts

--- src/express.ts
+++ src/express.ts
@@ -13,11 +13,14 @@ export default async function setup(app: Application): Promise<Application> {
         app.use(cors());
         app.options('*', cors());
       }
-    app.use(express.json())
+    app.use(express.json())    
+    app.use(express.urlencoded({ extended: true }))
+    app.use(cookieParser())
+    
     app.use("/api/v1/admin/auth", adminAuthRoutes);
 
     app.get('/', (req, res) =>{
-        res.send('Welcome to DTCE Server')
+        res.send('Welcome')
     })
     app.use('/404', (req, res) =>{
         res.status(404).send({message: 'Route not found'})
@@ -35,10 +38,6 @@ export default async function setup(app: Application): Promise<Application> {
 
         next()
     })
-    
-  app.use(express.urlencoded({ extended: true }))
-  app.use(express.json())
-  app.use(cookieParser())
 
   app.use((req: Request, res: Response, next: NextFunction) => {
     // Allow requests from all origins
